827bf547bf2e9b1560c6731c67c9e52fb3e260f4
Sreeharika <sreeharikayelle@gmail.com>
2020-09-17 19:17:51 +0530

    first commit

---
+++ amaze-frontend/src/App.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import './App.css';
+import './index.css';
+import data from './data'
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import HomeScreen from './components/HomeScreen';
+import ProductScreen from './components/ProductScreen';
+
+function App(){
+
+  const openMenu = ()=>{
+    document.querySelector('.sidebar').classList.add('open');
+  }
+  const closeMenu = ()=>{
+    document.querySelector('.sidebar').classList.remove('open');
+  }
+
+  return(
+    <BrowserRouter>
+    <div className='grid-container'>
+      <header className='header'>
+        <div className='brand'>
+          <button onClick={openMenu}>
+            &#9776;
+          </button>
+          <Link to='/' >AMAZE ECOM</Link>
+        </div>
+        <div className='header-links'>
+          <a href='Signin.html'>Signin</a>
+          <a href='Cart.html'>Cart</a>
+        </div>
+      </header>
+      <aside className='sidebar'>
+        <h3>Shooping Categories</h3>
+        <button className='sidebar-close-btn' onClick={closeMenu}>X</button>
+        <ul>
+          <li>
+            <a href='index.html'>Shirts</a>
+          </li>
+          <li>
+          <a href='index.html'>T-Shirts</a>
+          </li>
+          <li>
+          <a href='index.html'>Pants</a>
+          </li>
+        </ul>
+        </aside>
+      <main className='main'>
+        <div className='content'>
+        <Route path="/product/:id"  component={ProductScreen} />
+        <Route path="/" exact={true} component={HomeScreen} />
+       
+        </div>
+      </main>
+      <footer className='footer'>
+        All right reserved
+      </footer>
+
+    </div>
+    </BrowserRouter>
+  )
+}
+
+export default App;
---
+++ amaze-frontend/src/data.js
@@ -0,0 +1,87 @@
+export default {
+    products: [
+        {
+        _id:'1',
+        name: 'Slim shrit',
+        category: 'Shrits',
+        image: '/images/d1.jpg',
+        price: 599,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: 10
+        },
+        {
+            _id:'2',
+            name: 'Slim shrit',
+            category: 'Shrits',
+            image: '/images/d2.jpg',
+            price: 799,
+            brand: 'Polo',
+            rating: 4,
+            numReviews: 100
+        },
+        {
+            _id:'3',
+            name: 'Slim shrit',
+            category: 'Shrits',
+            image: '/images/d3.jpg',
+            price: 399,
+            brand: 'Max',
+            rating: 3.5,
+            numReviews: 20
+        },
+        {
+            _id:'4',
+            name: 'T-Shrit',
+            category: 'T-Shrits',
+            image: '/images/t1.jpg',
+            price: 999,
+            brand: 'Max',
+            rating: 4.5,
+            numReviews: 15
+        },
+        {
+            _id:'5',
+            name: 'T-Shrit',
+            category: 'T-Shrits',
+            image: '/images/t2.jpg',
+            price: 385,
+            brand: 'USpolo',
+            rating: 4,
+            numReviews: 105
+        },
+        {
+            _id:'6',
+            name: 'T-Shrit',
+            category: 'T-Shrits',
+            image: '/images/t3.jpg',
+            price: 599,
+            brand: 'GAP',
+            rating: 4.5,
+            numReviews: 5
+        },
+        {
+            _id:'7',
+            name: 'Pants',
+            category: 'Pants',
+            image: '/images/p1.jpg',
+            price: 1099,
+            brand: 'Levis',
+            rating: 3.5,
+            numReviews: 10
+        },
+        {
+            _id:'8',
+            name: 'Pants',
+            category: 'Pants',
+            image: '/images/p2.jpg',
+            price: 1299,
+            brand: 'Spykar',
+            rating: 4,
+            numReviews: 0
+        },
+               
+
+    ]
+}
+
